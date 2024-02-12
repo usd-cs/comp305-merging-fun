93fad871090541875124a7439c9d63317c42ab2f
Max Jeruss <mjeruss@sandiego.edu>
2024-02-12 15:05:54 -0800

    <WIP> feat: added print statement to main

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Olsen \nHello, Max Jeruss");
+        System.out.println("Hello, Liberty the Best Dog");
     }
 }
