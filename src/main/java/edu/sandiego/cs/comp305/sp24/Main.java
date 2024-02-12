dbcc23c7aa01d4e76f343b4a87bf5dfbcbe0af8f
Andrew Albizati <andrew.albizati@gmail.com>
2024-02-12 15:09:56 -0800

    feat: added andrew's name

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -4,11 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Dr. Olsen");
-<<<<<<< HEAD
+        System.out.println("Hello, Andrew");
         System.out.println("Hello, Sebastian");
-=======
         System.out.println("Hello, Wanda");
         System.out.println("Hello, Gianpaolo");
->>>>>>> e142737daed14b67e12db57f18b505618e4dd919
     }
 }
