9a056ef1fa79afe1bd6a54fad147f42da281fb7f
Andrew Albizati <andrew.albizati@gmail.com>
2024-02-12 14:57:11 -0800

    feat: added my own name

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -2,7 +2,7 @@ package edu.sandiego.cs.comp305.sp24;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello, Dr. Olsen");
+        System.out.println("Hello, Andrew");
     }
 }
