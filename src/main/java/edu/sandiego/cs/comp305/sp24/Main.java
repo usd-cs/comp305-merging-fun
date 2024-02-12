6cb75bdb8eb96ed862e489475ce1eeef9f248d00
natjungquist <njungnquist@sandiego.edu>
2024-02-12 15:02:36 -0800

    attempt to write a new name in a new branch

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -3,6 +3,6 @@ package edu.sandiego.cs.comp305.sp24;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, Natalie");
+        System.out.println("Hello, class");
     }
 }
