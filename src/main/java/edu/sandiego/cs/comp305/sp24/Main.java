d1500cef5474afaf19199e17ab1101ee285f4320
aescudero <aescudero@sandiego.edu>
2024-02-13 15:12:48 -0800

    Added second hello world entry

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println("Hello, Wanda");
         System.out.println("Hello, Gianpaolo");
         System.out.println("Hello, Augusto");
-
+        System.out.println("Hello, A");
     }
 }
