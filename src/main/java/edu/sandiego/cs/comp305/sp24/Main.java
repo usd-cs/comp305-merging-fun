c4de92b47d168329f829dfe618bba333a03a5633
Casey Klutznick <cklutznick@sandiego.edu>
2024-02-13 15:26:26 -0800

    add name

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -15,5 +15,6 @@ public class Main {
         System.out.println("Hello, Kobe");
         System.out.println("Hello, Thomas");
         System.out.println("Hello, Audrey");
+        System.out.println("Hello, Casey");
     }
 }
