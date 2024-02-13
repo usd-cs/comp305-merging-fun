241d9a813b297595dff0fad2ddf9104730149f30
Audrey Naidu <anaidu@sandiego.edu>
2024-02-13 15:01:52 -0800

    feat: printed hello to someone else in Main

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -12,6 +12,7 @@ public class Main {
         System.out.println("Hello, Wanda");
         System.out.println("Hello, Gianpaolo");
         System.out.println("Hello, Audrey");
+        System.out.println("Hello, person1");
 
     }
 }
