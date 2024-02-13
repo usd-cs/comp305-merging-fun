7c3037f5f3b0ab4d57d8ac77f22ed2a8f43428c6
Cristian St.Clair <cstclair@sandiego.edu>
2024-02-13 15:02:49 -0800

    feat: Updated Main

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -4,13 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, Cristian");
+        System.out.println("Hello, Mom!");
         System.out.println("Hello, Dr. Olsen");
         System.out.println("Hello, class!");
-        System.out.println("Hello, Kyle W.");
-        System.out.println("Hello, Andrew");
-        System.out.println("Hello, Sebastian");
-        System.out.println("Hello, Wanda");
-        System.out.println("Hello, Gianpaolo");
 
     }
 }
