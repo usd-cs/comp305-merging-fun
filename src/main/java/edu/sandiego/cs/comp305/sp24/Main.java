2b997bb52415ff2243a2820521bf2ef5ef54317f
Tin Aung <taung@sandiego.edu>
2024-02-13 15:07:31 -0800

    new print statement added

--- src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
+++ src/main/java/edu/sandiego/cs/comp305/sp24/Main.java
@@ -12,6 +12,6 @@ public class Main {
         System.out.println("Hello, Wanda");
         System.out.println("Hello, Gianpaolo");
         System.out.println("Hello, Tin");
-
+        System.out.println("Hello, comp305!");
     }
 }
